275115772cba3b5ce35050a1999d2c169e2c76e4
Boris <jackofdiamond596@gmail.com>
2018-05-17 16:07:24 +0300

    Updated Widget

--- Widget/infragistics.ui.widget.js
+++ Widget/infragistics.ui.widget.js
@@ -23,36 +23,73 @@ if (typeof jQuery !== "function") {
 	$.widget('ui.igWidget', {
 		css: {
 			/* igWidget element classes go here */
+			container: 'widget',
+			counter: 'count'
 		},
         options: {
 			/* igWidget options go here */
 			startValue : 10,
-			currentValue : 10,
+			currentValue : null,
 			stopMessage : "Stopped",
 			pauseMessage : "Paused",
 			autoStart : false,
 			delta : 1
         },
 		events: {
-			/* igWidget events go here */
-			// on elapsed
-			// on tick
-			// rendering
-			// rendered
-			// started
-			// stopped
-			// paused
-			// resumed
+			elapsed: new Event('elapsed'),
+			tick: new Event('tick'),
+			rendering: new Event('rendering'),
+			rendered: new Event('rendered'),
+			started: new Event('started'),
+			stopped: new Event('stopped'),
+			paused: new Event('paused'),
+			resumed: new Event('resumed')
 		},
 		start: function (curValue) {
-			//return this._intervalID = setInterval($.proxy(this.decrement, this, true), 1000);
-			return 'started';
+			// if started is triggered -> return -> otherwise it will increase countdown speed by 1 sec
+			this._beginCountdown();
 		},
 		pause: function () {
-			return 'paused'	
+			// TODO: trigger paused
+			console.log('paused');
 		},
 		stop: function () {
-			return 'stopped'
+			// TODO: trigger stopped
+			console.log('stopped');
+		},
+		_render: function () {
+			// TODO: trigger rendering
+			var div = $('<div />');
+			div.append($('<span />'));
+			div.addClass(this.css.container);
+			
+			this.element.prepend(div);
+			
+			var span = $('.widget > span');
+			span.addClass('counter');
+			span.on('elapsed', function() {console.log('pesho')})
+
+			this.options.currentValue = this.options.startValue;
+
+			span.text(this.options.currentValue);
+			// TODO: trigger rendered
+		},
+		_beginCountdown: function() {
+			// TODO: trigger started; if paused was triggered - trigger resumed instead of started
+			this._intervalID = setInterval($.proxy(this._updateCurrentValue, this, true), 1000);
+		},
+		_updateCurrentValue: function () {
+			var counter = $('.counter');
+			this.options.currentValue -= this.options.delta;
+
+			if(this.options.currentValue <= 0) {
+				counter.text(0);
+				// TODO: trigger elapsed
+
+				return;
+			}
+			
+			counter.text(this.options.currentValue);
 		},
         _create: function () {
 			/* igWidget constructor goes here */
@@ -61,7 +98,12 @@ if (typeof jQuery !== "function") {
 			this.options.pauseMessage = this.options.pauseMessage;
 			this.options.delta = this.options.delta;
 			this.options.autoStart = this.options.autoStart;
-			this.element.addClass( "widget" );
+
+			this._render();
+
+			if(this.options.autoStart) {
+				this._beginCountdown();
+			}
         },
         _setOption: function (option, value) {
 			/* igWidget custom setOption goes here */
