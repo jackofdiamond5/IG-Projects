8212988132e9b8eb7c49196f2c11e02f6659670a
AMarinov <amarinov@infragistics.com>
2018-08-23 16:30:54 +0300

    Adding the Open ID demo with the MS demo server

--- LoginView/src/app/services/authentication.service.ts
+++ LoginView/src/app/services/authentication.service.ts
@@ -1,17 +1,49 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { OnInit, OnDestroy, Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, Subscription } from 'rxjs';
 import { LoginResultModel } from '../models/LoginResultModel';
 import { User } from '../models/UserModel';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthenticationService {
+export class AuthenticationService implements OnInit, OnDestroy {
 
-  constructor(private http: HttpClient) {
+  isAuthorized: boolean;
+  isAuthorizedSubscription: Subscription;
+  apiResult: string;
 
+  constructor(private http: HttpClient, private oidcSecurityService: OidcSecurityService) {
+    this.isAuthorized = false;
   }
+
+  ngOnInit() {
+    this.isAuthorizedSubscription = this.oidcSecurityService.getIsAuthorized()
+      .subscribe(isAuthorized => this.isAuthorized = isAuthorized);
+   }
+
+  ngOnDestroy() {
+    this.isAuthorizedSubscription.unsubscribe();
+  }
+
+  //signUp() {
+  //  this.oidcSecurityService.authorize();
+  //}
+
+  //logout() {
+  //  this.oidcSecurityService.logoff();
+  //}
+
+  //callApi() {
+  //  const token = this.oidcSecurityService.getToken();
+   // const apiURL = 'https://fabrikamb2chello.azurewebsites.net/hello';
+   // const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+//
+   // this.http.get(apiURL, { headers: headers }).subscribe(
+   //     response => this.apiResult = JSON.stringify(response),
+   //     error => console.log(error));
+  //}
 
   login(email: string, password: string): Observable<LoginResultModel> {
     return this.http.post<LoginResultModel>('https://reqres.in/api/login', {
