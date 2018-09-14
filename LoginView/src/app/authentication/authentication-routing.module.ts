71b10515a1b5584cdcf068071ee4322fb058d3d8
AMarinov <amarinov@infragistics.com>
2018-09-14 13:23:37 +0300

    Fixing the auth routes

--- LoginView/src/app/authentication/authentication-routing.module.ts
+++ LoginView/src/app/authentication/authentication-routing.module.ts
@@ -1,6 +1,5 @@
-
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+import {NgModule, ModuleWithProviders} from '@angular/core';
 
 import { AuthGuard } from './services/auth.guard';
 import { ProfileComponent } from './profile/profile.component';
@@ -8,14 +7,18 @@ import { RedirectComponent } from './redirect/redirect.component';
 import { ExternalAuthProvider } from './services/igx-auth.service';
 
 const authRoutes: Routes = [
-
     { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
-
-  { path: 'redirect-google', component: RedirectComponent, data: { provider: ExternalAuthProvider.Google } },
-  { path: 'redirect-facebook', component: RedirectComponent, data: { provider: ExternalAuthProvider.Facebook } },
-  { path: 'redirect-microsoft', component: RedirectComponent, data: { provider: ExternalAuthProvider.Microsoft } }
+    { path: 'redirect-google', component: RedirectComponent, data: { provider: ExternalAuthProvider.Google } },
+    { path: 'redirect-facebook', component: RedirectComponent, data: { provider: ExternalAuthProvider.Facebook } },
+    { path: 'redirect-microsoft', component: RedirectComponent, data: { provider: ExternalAuthProvider.Microsoft } }
 ];
 
 @NgModule({
+    imports: [
+      RouterModule.forChild(authRoutes)
+    ],
+    exports: [
+      RouterModule
+    ]
+  })
 export class AuthenticationRoutingModule {}
-
